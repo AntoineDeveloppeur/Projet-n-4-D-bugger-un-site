11a17c84906d82abcdfc8f5b522ea9e9a1168b13
Antoine V <antoine.verove@gmail.com>
2024-04-27 12:52:17 +0200

    Suite à l’analyse de wave,J’ai ajouté un label vide pour l’input submit et J’ai changé en noir la couleur des textes des filtres (à la barbare en ajoutant style=color: black; dans la fonction : showItemTags. Car je ne connais pas la librery bootsrap ou framework. Je ne sais pas comment changer les propriétés d’une classe

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -220,10 +220,10 @@
     },
     showItemTags(gallery, position, tags) {
       var tagItems =
-        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
+        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all" style="color: black;">Tous</span></li>';
       $.each(tags, function(index, value) {
         tagItems += `<li class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
+                <span class="nav-link" style="color: black;" data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
 
